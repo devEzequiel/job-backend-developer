c39af42b86d1496df61cf03d3af7b95300e9778c
Zekke <esofinney@gmail.com>
2023-07-14 09:01:44 -0300

    [ft/product] - atualizado o controller base

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,11 +3,88 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
+use Symfony\Component\HttpFoundation\Response;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, ValidatesRequests;
+
+    /**
+     * @param string $message
+     * @return JsonResponse
+     */
+    public function responseOk($data, string $message = 'success'): JsonResponse
+    {
+        return response()->json(['message' => $message, 'data' => $data], Response::HTTP_OK);
+    }
+
+    /**
+     * @param string $message
+     * @return JsonResponse
+     */
+    public function responseCreated($data, string $message = ''): JsonResponse
+    {
+        return response()->json(['message' => $message, "data" => $data], Response::HTTP_CREATED);
+    }
+
+    /**
+     * @param string $message
+     * @return JsonResponse
+     */
+    public function responseAccepted(string $message = 'success'): JsonResponse
+    {
+        return response()->json(['message' => $message], Response::HTTP_ACCEPTED);
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function responseUnauthorized(): JsonResponse
+    {
+        return response()->json(null, Response::HTTP_UNAUTHORIZED);
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function responseForbidden(): JsonResponse
+    {
+        return response()->json(null, ResponseAlias::HTTP_FORBIDDEN);
+    }
+
+    /**
+     * @param string $message
+     * @return JsonResponse
+     */
+    public function responseNotFound(string $message = ''): JsonResponse
+    {
+        return response()->json($message ? ['message' => $message] : null, Response::HTTP_NOT_FOUND);
+    }
+
+    /**
+     * @param string $message
+     * @return JsonResponse
+     */
+    public function responseUnprocessableEntity(string $message = 'error'): JsonResponse
+    {
+        return response()->json(
+            $message ? ['message' => $message] : null,
+            Response::HTTP_UNPROCESSABLE_ENTITY
+        );
+    }
+
+    /**
+     * @param string $message
+     * @return JsonResponse
+     */
+    public function responseInternalServerError(string $message = 'error'): JsonResponse
+    {
+        return response()->json(
+            $message ? ['message' => $message] : null,
+            Response::HTTP_INTERNAL_SERVER_ERROR
+        );
+    }
 }
