0fe0053d40b95432ff2479ece90e789f1b77a0b8
Zekke <esofinney@gmail.com>
2023-07-14 09:02:40 -0300

    [ft/product] - criado os requests de produto

---
+++ app/Http/Requests/Product/StoreProductRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests\Product;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string', 'unique:products,name'],
+            'price' => ['required', 'numeric'],
+            'description' => ['required', 'string'],
+            'category' => ['required', 'string'],
+            'image_url' => ['nullable', 'string']
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'O campo nome é obrigatório.',
+            'name.string' => 'O campo nome deve ser uma string.',
+            'name.unique' => 'O nome já está em uso.',
+            'price.required' => 'O campo preço é obrigatório.',
+            'price.numeric' => 'O campo preço deve ser um número.',
+            'description.required' => 'O campo descrição é obrigatório.',
+            'description.string' => 'O campo descrição deve ser uma string.',
+            'category.required' => 'O campo categoria é obrigatório.',
+            'category.string' => 'O campo categoria deve ser uma string.',
+            'image_url.string' => 'A URL da imagem deve ser uma string.'
+        ];
+    }
+}
